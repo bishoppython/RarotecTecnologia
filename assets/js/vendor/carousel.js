fcb576077d06559c0ce2e343fc3a35fe99924c1b
Anderson Bispo dos Santos <bishoppython@gmail.com>
2020-12-09 14:43:24 -0300

    Add files via upload

---
+++ assets/js/vendor/carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#owl-demo").owlCarousel({
+      autoPlay: 1000, //Set AutoPlay to 3 seconds
+      dots: true,
+      items: 2,
+      itemsDesktop: [1199, 3],
+      itemsDesktopSmall: [979, 3]
+    });
+  });
